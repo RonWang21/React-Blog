36e655b3ebfdec6d238f9f472241de70b92adc7a
my name <mushencjf@gmail.com>
2019-12-13 10:06:53 +0800

    updata Home

--- src/containers/home/Home.jsx
+++ src/containers/home/Home.jsx
@@ -3,13 +3,8 @@ import './Home.less'
 class Home extends Component {
   render() {
     return (
-<<<<<<< HEAD:src/containers/Home/Home.jsx
       <div className="box1">
         <h1>我是home组件</h1>
-=======
-      <div>
-        <h1>我是Home组件???</h1>
->>>>>>> b613243c7c5ae58c079fd29f9b363e04d343c7af:src/containers/home/Home.jsx
       </div>
     )
   }
