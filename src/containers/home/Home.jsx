b3af2d0f9a02bc1f2edf491e649c2571250fd52d
my name <mushencjf@gmail.com>
2019-12-14 01:18:54 +0800

    deleted header & add homeTags & add homeSidesbar & add homeDetail

--- src/containers/home/Home.jsx
+++ src/containers/home/Home.jsx
@@ -1,16 +1,18 @@
 import React, { Component } from 'react'
 import './Home.less'
-// 引入header组件
-import Header from '../../components/header/Header.jsx'
+// 引入HomeTags组件
+import HomeTags from '../../components/home/homeTags/HomeTags'
+import HomeSidebar from '../../components/home/homeSidebar/HomeSidebar'
+import { Layout } from 'element-react'
+
 class Home extends Component {
   render() {
     return (
       <div>
-        <Header />
-        <div className="home-contanier">
-          <div className="home-main-contanier">home-mian</div>
-          <div className="home-sidebar-contanier">home-sidebar</div>
-        </div>
+        <Layout.Row type="flex" justify="center">
+          <HomeTags />
+          <HomeSidebar />
+        </Layout.Row>
       </div>
     )
   }
