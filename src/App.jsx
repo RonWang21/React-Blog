28d7e54721b2fb39f662b6c0e26fdf22897e097d
LD382549823 <382549823@qq.com>
2019-12-13 19:45:40 +0800

    fix bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,5 @@
 import React, { Component } from 'react'
 // 引入Home组件
-<<<<<<< HEAD
-import SideNav from './containers/sideNav/SideNav'
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <SideNav />
-      </div>
-=======
 // import Home from './containers/home/Home'
 //引入路由组件
 import router from './config/routes'
@@ -34,7 +25,6 @@ class App extends Component {
           <Route component={NotMath}></Route>
         </Switch>
       </Router>
->>>>>>> master
     )
   }
 }
