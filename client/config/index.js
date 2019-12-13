fa56b6e24859a6072212cf9cb55d131237e84e8b
ron <luckron98@gmail.com>
2019-12-14 01:26:13 +0800

    added REAMDE English version1.0 & add server with login/register API & some update

--- client/config/index.js
+++ client/config/index.js
@@ -2,16 +2,20 @@ let DB_CONFIG, SERVER_CONFIG
 
 // 数据库链接配置
 DB_CONFIG = {
-  port : 27017,
-  host:'localhost'
+  port: 27017,
+  host: 'localhost'
 }
 
 // 服务器链接配置
 SERVER_CONFIG = {
-  port : 1998
+  port: 1998
 }
+
+// jsonwebpack插件需要的秘钥
+const PRIVARE_KEY = 'Galaxy Defender'
 
 module.exports = {
   DB_CONFIG,
-  SERVER_CONFIG
+  SERVER_CONFIG,
+  PRIVARE_KEY
 }
