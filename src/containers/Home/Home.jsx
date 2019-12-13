36e4a1b7c2ec05e89866b8eae66b12a8ed60ac8a
my name <mushencjf@gmail.com>
2019-12-13 09:22:58 +0800

    mushen

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -1,9 +1,9 @@
 import React, { Component } from 'react'
-
+import './Home.less'
 class Home extends Component {
   render() {
     return (
-      <div>
+      <div className="box1">
         <h1>我是home组件</h1>
       </div>
     )
