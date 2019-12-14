307003cffb8bf779abf2e1cf86cda2c082038da3
ron <luckron98@gmail.com>
2019-12-14 14:14:43 +0800

    update

--- src/components/home/homeSidebar/HomeSidebar.jsx
+++ src/components/home/homeSidebar/HomeSidebar.jsx
@@ -1,6 +1,11 @@
 import React, { Component } from 'react'
+
+import RecomArticle from './recomArticle/RecomArticle'
+import Login from './login/Login'
+
 import { Layout } from 'element-react'
 import './HomeSidebar.less'
+
 class HomeSidebar extends Component {
   render() {
     return (
@@ -8,7 +13,12 @@ class HomeSidebar extends Component {
         <Layout.Row type="flex">
           <Layout.Col span="1" offset="1">
             <div className="grid-content bg-purple">
-              <div className="home-sidebar-contanier">home-sidebar</div>
+              <div className="home-sidebar-contanier">
+                <div className="home-sidebar-contanier">
+                  <Login />
+                  <RecomArticle />
+                </div>
+              </div>
             </div>
           </Layout.Col>
         </Layout.Row>
