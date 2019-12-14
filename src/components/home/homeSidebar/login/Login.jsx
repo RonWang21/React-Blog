9c88a2342d519d1bae94e4b5d1a63c0b7823bfa8
ron <luckron98@gmail.com>
2019-12-14 14:42:05 +0800

    update login component

--- src/components/home/homeSidebar/login/Login.jsx
+++ src/components/home/homeSidebar/login/Login.jsx
@@ -1,13 +1,15 @@
 import React, { Component } from 'react'
 
-class Home extends Component {
+import { Tabs } from 'element-react'
+class Login extends Component {
   render() {
     return (
-      <div>
-        <h1>我是login组件???</h1>
-      </div>
+      <Tabs activeName="2" onTabClick={tab => console.log(tab.props.name)}>
+        <Tabs.Pane activeName="login" label="登录" name="1"></Tabs.Pane>
+        <Tabs.Pane activeName="register" label="注册" name="2"></Tabs.Pane>
+      </Tabs>
     )
   }
 }
 
-export default Home
+export default Login
