c3fd5ec7ec928c839a0701a624bba2234a9aeea2
LD382549823 <382549823@qq.com>
2019-12-14 11:44:42 +0800

    Sorrow sideNav components distribute

--- src/containers/home/Home.jsx
+++ src/containers/home/Home.jsx
@@ -1,19 +1,21 @@
-import React, { Component } from 'react'
-import './Home.less'
+import React, { Component } from "react";
+import "./Home.less";
 // 引入header组件
-import Header from '../../components/header/Header.jsx'
+import Header from "../../components/header/Header.jsx";
+// 引入sideNev组件
+import SideNav from "../../containers/sideNav/SideNav";
 class Home extends Component {
   render() {
     return (
       <div>
         <Header />
         <div className="home-contanier">
-          <div className="home-main-contanier">home-mian</div>
-          <div className="home-sidebar-contanier">home-sidebar</div>
+          <div className="home-main-contanier" />
+          <SideNav />
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default Home
+export default Home;
