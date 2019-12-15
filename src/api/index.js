3254933620afcfa7e1655be8a5f22042742a8c0c
LeeJay9701 <LeeJay9701@email.com>
2019-12-15 21:25:43 +0800

    test login

--- src/api/index.js
+++ src/api/index.js
@@ -1,14 +1,41 @@
 import ajax from './ajax'
-const url = `/api`
+const url = `/api/v1.0`
 export const reqUser = () =>
   ajax({
     method: 'get',
     url: url + '/userlist'
   })
-export const reqGetTags = () =>
+export const reqGetTags = id =>
   ajax({
     method: 'get',
-    url: url + '/getTags'
+    url: url + '/getTags',
+    data: {
+      id
+    }
+  })
+export const reqGetCategories = () =>
+  ajax({
+    method: 'get',
+    url: url + '/getCategories'
+  })
+
+export const reqAddCategory = ({ categoryname, tags }) =>
+  ajax({
+    method: 'post',
+    url: url + '/addCategory',
+    data: { categoryname, tags }
+  })
+export const reqUpdateCategory = ({ categoryname, id }) =>
+  ajax({
+    method: 'post',
+    url: url + '/updateCategory',
+    data: { categoryname, id }
+  })
+export const reqDelCategory = id =>
+  ajax({
+    method: 'post',
+    url: url + '/delCategory',
+    data: { id }
   })
 export const reqAddTag = ({ tagname }) =>
   ajax({
@@ -34,4 +61,19 @@ export const requpdateTag = ({ tagname, newname }) =>
       tagname,
       newname
     }
+  })
+// export const requpdateTag = ({ tagname, newname }) =>
+//   ajax({
+//     method: 'post',
+//     url: url + '/updateTag',
+//     data: {
+//       tagname,
+//       newname
+//     }
+//   })
+export const reqLogin = ({ username, password }) =>
+  ajax({
+    method: 'post',
+    url: url + '/login',
+    data: { username, password }
   })
