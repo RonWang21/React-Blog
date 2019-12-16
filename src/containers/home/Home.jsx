e85ae2345dc9830fbc9cfe5911e1dd0a3311daa3
LeeJay9701 <LeeJay9701@email.com>
2019-12-16 21:26:56 +0800

    why

--- src/containers/home/Home.jsx
+++ src/containers/home/Home.jsx
@@ -9,7 +9,6 @@ import { Layout } from 'element-react'
 class Home extends Component {
   render() {
     return (
-<<<<<<< HEAD
       <Layout.Row className="view-container" justify="center">
         {/* header--start */}
         <Header />
@@ -19,19 +18,6 @@ class Home extends Component {
         <HomeMain children={this.props.children} />
         {/* main--end */}
       </Layout.Row>
-=======
-      // <Router>
-        <Layout.Row className="view-container" justify="center">
-          {/* header--start */}
-          <Header />
-          {/* header--end */}
-          {/* main--start */}
-          {/* 由于HomeMain在Home中，在App中定义路由的时候将写在HomeMain里的路由组件变成了Home的子组件 */}
-          <HomeMain children={this.props.children} />
-          {/* main--end */}
-        </Layout.Row>
-      // </Router>
->>>>>>> 7e7ac2bccf13110b22d85e2f249d84a31baa4b77
     )
   }
 }
